3dbcf3735b603e41f774ae5ed1e26a65bf8fde7a
Camilo Velasco Rueda <boterito3@gmail.com>
2021-12-27 16:24:02 -0500

    Add api

---
+++ 57Blocks/57Blocks.api/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _57Blocks.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Register")]
+        public ActionResult<string> Register(IFormFile file)
+        {
+            return "OK";
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public ActionResult<string> Login(string parameter)
+        {
+            return "OK";
+        }
+    }
+}
